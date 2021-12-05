7e64084c342eda65f42ee34aee9db8e9e6c8e872
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2021-12-05 19:45:00 +0300

    update test.py

--- test.py
+++ test.py
@@ -3,18 +3,16 @@ from pathlib import Path
 
 from argparse_dataclass import ArgumentParser
 import torch
+import torchaudio.pipelines as pipelines
 from torchinfo import summary
 import wandb
 
 from src.aligners import GraphemeAligner
 from src.configs import FastSpeech2Config
-from src.data_utils import collate, LJSpeechDataset, Wav2Spec
+from src.data_utils import Wav2Spec
 from src.models import FastSpeech
 from src.utils import seed_all
 from src.vocoders import WaveGlow
-
-
-VAL_INDICES = 'data/lj_speech/val_indices.txt'
 
 
 seed_all()
@@ -26,59 +24,44 @@ summary(fs)
 
 aligner = GraphemeAligner(config)
 
-val_ds = LJSpeechDataset(aligner, root=config.data_dir,
-                         indices_path=VAL_INDICES)
-
-val_loader = torch.utils.data.DataLoader(
-    val_ds,
-    batch_size=config.val_batch_size,
-    shuffle=True,
-    collate_fn=collate,
-    num_workers=config.val_num_workers
-)
+ds = [
+    ('A defibrillator is a device that gives a high energy electric shock ' +
+     'to the heart of someone who is in cardiac arrest').lower(),
+    ('Massachusetts Institute of Technology may be best known for its math, ' +
+     'science and engineering education').lower(),
+    ('Wasserstein distance or Kantorovich Rubinstein metric ' +
+     'is a distance function defined between probability distributions ' +
+     'on a given metric space').lower()
+]
 
 wav2spec = Wav2Spec(config)
 vocoder = WaveGlow(config.device)
+tokenizer = pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
 device = config.device
 
 wandb.init(job_type='test-model', config=config)
-a = wandb.use_artifact('trainer_state:best')
+a = wandb.use_artifact('_username_/fast_speech/trainer_state:best',
+                       type='trainer-state')
 state_path = Path(a.download()) / 'state.pth'
 state_dict = torch.load(state_path, map_location=config.device)
 fs.load_state_dict(state_dict['model'])
+fs.eval()
 
-table = wandb.Table(columns=(
-    'ground_truth_spec',
+table = wandb.Table(columns=[
     'output_spec',
-    'ground_truth_wav',
     'output_wav',
     'text'
-))
+])
 
-for batch in val_loader:
-    out_specs, out_lengths = fs(batch.tokens.to(device))
-    out_lengths = out_lengths.round().int()
+for text in ds:
+    tokens, _ = tokenizer(text)
+    out_spec, _ = fs(tokens.to(device))
+    out_wav = vocoder.inference(out_spec).squeeze().cpu()
 
-    gt_specs = wav2spec(batch.waveform.to(device))
-    gt_lengths = wav2spec.transform_lengths(batch.waveform_length.to(device)) \
-        .round().int()
+    table.add_data(
+        wandb.Image(out_spec.cpu()),
+        wandb.Audio(out_wav, sample_rate=vocoder.OUT_SAMPLE_RATE),
+        wandb.Html(text)
+    )
 
-    transcript = batch.transcript
-
-    assert (len(out_specs) == len(out_lengths) ==
-            len(gt_specs) == len(gt_lengths) == len(transcript))
-    bs = len(out_specs)
-    for index in range(bs):
-        gt_spec = gt_specs[index, :, :gt_lengths[index]]
-        out_spec = out_specs[index, :, :out_lengths[index]]
-        gt_wav = vocoder.inference(gt_spec.unsqueeze(0)).squeeze().cpu()
-        out_wav = vocoder.inference(out_spec.unsqueeze(0)).squeeze().cpu()
-
-        table.add_data(
-            wandb.Image(gt_spec.cpu()),
-            wandb.Image(out_spec.cpu()),
-            wandb.Audio(gt_wav, sample_rate=vocoder.OUT_SAMPLE_RATE),
-            wandb.Audio(out_wav, sample_rate=vocoder.OUT_SAMPLE_RATE),
-            wandb.Html(transcript[index])
-        )
 wandb.log({'val results': table})
